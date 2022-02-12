efb2176c1d686e988beabcac02eece7d141f6048
shah1854 <shah1854@students.d211.org>
2022-02-11 22:06:07 -0600

    tester

--- app/src/main/java/com/example/test/Register.java
+++ app/src/main/java/com/example/test/Register.java
@@ -1,5 +1,6 @@
 package com.example.test;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
@@ -8,7 +9,17 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.common.internal.Objects;
+import com.google.firebase.FirebaseApiNotAvailableException;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class Register extends AppCompatActivity {
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://chatapp-faa46-default-rtdb.firebaseio.com/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +42,27 @@ public class Register extends AppCompatActivity {
                 Toast.makeText(Register.this, "All Fields Required!", Toast.LENGTH_SHORT).show();
             }
             else{
+                databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.child("users").hasChild(mobileTxt)){
+                            Toast.makeText(Register.this, "Mobile already exists", Toast.LENGTH_SHORT).show();
+                        }else{
+                            databaseReference.child("users").child(mobileTxt).child("email").setValue(emailTxt);
+                            databaseReference.child("users").child(mobileTxt).child("name").setValue(nameTxt);
 
+                            Toast.makeText(Register.this, "Success", Toast.LENGTH_SHORT).show();
+
+                            Intent intent = new Intent(Register.this, MainActivity.class);
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
             }
         }
     });
